389f8acb84d9d106aa9900800df9e20a167ed540
Hoseong Hwang <thefron@42.company>
2018-08-06 01:34:46 +0900

    Add empty home view

---
+++ ui/src/components/MigrationsInProgress.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-data-table
+    :items="migrations"
+  >
+    <template slot="headers" slot-scope="props">
+      <tr>
+        <th>id</th>
+        <th>started at</th>
+        <th>cluster name</th>
+        <th>requestor</th>
+        <th>alter statement</th>
+        <th>approved by</th>
+        <th>copy progress</th>
+        <th>status</th>
+      </tr>
+    </template>
+    <template slot="items" slot-scope="props">
+    </template>
+  </v-data-table>
+</template>
+
+<script>
+export default {
+  name: 'MigartionsInProgress',
+  props: {
+    migrations: []
+  }
+};
+</script>
--- ui/src/views/Home.vue
+++ ui/src/views/Home.vue
@@ -2,10 +2,25 @@
   <v-container fluid>
     <v-slide-y-transition mode="out-in">
       <v-layout column align-center>
+        <img src="@/assets/gh-ost-logo-light-160.png" alt="gh-ost">
+        <v-subheader>Migrations in Progress</v-subheader>
+        <MigrationsInProgress/>
+        <v-subheader>Pending Migrations</v-subheader>
       </v-layout>
     </v-slide-y-transition>
   </v-container>
 </template>
+
+<script>
+import MigrationsInProgress from '@/components/MigrationsInProgress.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    MigrationsInProgress
+  }
+};
+</script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
