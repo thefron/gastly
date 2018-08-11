9048df2ec0da0fd0f379fc23fa46a4289a65d093
Hoseong Hwang <thefron@42.company>
2018-08-11 15:28:19 +0900

    Setup dep

--- runner/cmd/runner/main.go
+++ runner/cmd/runner/main.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"flag"
-	"fmt"
-	"log"
 	"os/exec"
+
+	"github.com/golang/glog"
 
 	"github.com/Buzzvil/gastly/runner/pkg/migration"
 )
@@ -14,17 +14,19 @@ type runner struct {
 }
 
 func (runner *runner) execGhost(migration *migration.Migration) (bool, error) {
+	glog.Infof("mig_id=%d: Running %s", migration.Id, runner.GhostPath)
 	cmd := exec.Command(runner.GhostPath)
 	if err := cmd.Start(); err != nil {
-		log.Println(fmt.Errorf("exec error: %s", err))
+		glog.Errorf("exec error: %s", err)
 		return false, err
 	}
 	return true, nil
 }
 
 func main() {
-	runner := new(runner)
-	flag.StringVar(&runner.GhostPath, "ghost-path", "/gh-ost", "Path to gh-ost binary")
+	runner := &runner{}
+	flag.StringVar(&runner.GhostPath, "ghost-path", "/bin/gh-ost", "Path to gh-ost binary")
+	flag.Parse()
 
 	runner.execGhost(&migration.Migration{})
 }
